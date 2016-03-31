441c675e98d07fe45b2d8d378b4f8b89e938203e
Vinicius Valvassori <ViniciusValvassori@MacBook-Pro-de-Vinicius-2.local>
2016-03-31 14:53:23 -0300

    Segue for video, collection view work in progress

--- MC1/ViewController.swift
+++ MC1/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    let reuseIdentifier = "highlightsCellId"
+    @IBOutlet weak var hlCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,13 +24,49 @@ class ViewController: UIViewController {
                 print(produto.photos!)
             }
         }
+        
+        
+        hlCollectionView.delegate = self
+        hlCollectionView.dataSource = self
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(false)
+//        hlCollectionView.scrollToItemAtIndexPath(NSIndexPath.init(forItem: 1, inSection: 0), atScrollPosition: .CenteredHorizontally, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsetsMake(0, 200, 0, 200)
+    }
 
-
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = hlCollectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! HighlightCell
+        cell.highlightProductImage.image = UIImage(named: "running")
+        print(indexPath)
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        //vai para o video do produto
+    }
+    
+    func collectionView(collectionView: UICollectionView, didHighlightItemAtIndexPath indexPath: NSIndexPath) {
+        print(NSIndexPath)
+    }
 }
 
