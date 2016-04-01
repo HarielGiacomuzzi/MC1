133a9c57aa914c2d1035ab0c2cd48565695d7608
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2016-04-01 15:12:25 -0300

    + UISwipeGestureRecognizer, mas sumiu os controles do video...

--- MC1/ProductsViewController.swift
+++ MC1/ProductsViewController.swift
@@ -7,19 +7,26 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ProductsViewController: UIViewController {
     
-    var playerViewController:ProductPlayerViewController!
-
+    var playerLayer:AVPlayerLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.playerViewController = ProductPlayerViewController()
+        let url = NSURL(string: "https://apollo2.dl.playstation.net/cdn/UP3643/CUSA01608_00/FREE_CONTENTpybmPpILsFglhU1zQQYf/PREVIEW_GAMEPLAY_VIDEO_109467.mp4")
         
-        self.addChildViewController(self.playerViewController)
+        self.playerLayer = AVPlayerLayer(player: AVPlayer(playerItem: AVPlayerItem(URL: url!)))
+        self.playerLayer.frame = self.view.frame
+        self.view.layer.addSublayer(self.playerLayer)
+        self.playerLayer.player!.play()
         
-        self.playerViewController.loadView(self.view.frame)
+        let swipeRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipedUp))
+        swipeRecognizer.direction = .Up
+        self.view.addGestureRecognizer(swipeRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +34,9 @@ class ProductsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func swipedUp() {
+        self.playerLayer.frame = CGRect(x: 850, y: 90, width: 1000, height: 554)
+    }
 
     /*
     // MARK: - Navigation
